1038617799edfa97ebe13445aae81b17f6fc17ab
prathiksha3 <aprathiksha36@gmail.com>
2023-11-27 09:10:43 +0530

    first commit

---
+++ calculator_web.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero."
+
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    result = None
+
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operator = request.form['operator']
+
+        if operator == '+':
+            result = add(num1, num2)
+        elif operator == '-':
+            result = subtract(num1, num2)
+        elif operator == '*':
+            result = multiply(num1, num2)
+        elif operator == '/':
+            result = divide(num1, num2)
+
+    return render_template('calculator.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
